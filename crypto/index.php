19bee5d34144b7d0af202b56dd48b9a480fead9c
skibidigusiks <bujansgustavs@gmail.com>
2025-04-23 10:33:18 +0000

    pirmais

---
+++ crypto/index.php
@@ -0,0 +1,16 @@
+<?php
+  $data1 = file_get_contents("https://www.worldcoinindex.com/apiservice/json?key=eA1UQLojDkGOqxJiglMoE4Aw6ddVyITD80f");
+  $cryptoData1 = json_decode($data1, true);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
