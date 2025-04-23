15f03d30f6afd219c33e2de00425e2cab5668c92
Vivid21Monke <gibnersn123@gmail.com>
2025-04-23 11:25:59 +0000

    php ready

--- crypto/index.php
+++ crypto/index.php
@@ -8,9 +8,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Crypto</title>
 </head>
 <body>
-    
+<section class="Nav-Section">
+  <div class="Our-Logo/Name">
+    N/A
+  </div>
+  <div>
+    <button class="Main">N/A</button>
+    <button class="Info">N/A</button>
+    <button class="meme">N/A</button>
+  </div>
+</section>
+<section class="Main-crypto">
+  <div class="Big-section">
+    <div class="curency-big">
+      <div class="curency-individual">
+        N/A
+      </div>
+    </div>
+  </div>
+</section>
 </body>
 </html>
