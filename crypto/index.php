bcb60994ecd2370a829d4bf1374fde8fa95b8bbe
Vivid21Monke <gibnersn123@gmail.com>
2025-04-24 07:27:52 +0000

    adafa

--- crypto/index.php
+++ crypto/index.php
@@ -14,17 +14,31 @@
 <body>
   <div class="container">
 
-     <!--  navigation  -->
-    <div class="container__nav">
-      <div class="Our-Logo-Name">
-        N/A
-      </div>
-      <div class="conntainer__nav__buttons">
-        <button class="Main-Nav-Button">N/A</button>
-        <button class="Info-Nav-Button">N/A</button>
-        <button class="meme-Nav-Button">N/A</button>
-      </div>
+      <!--  navigation  -->
+      <div class="container__nav">
+        <div class="Our-Logo-Name">
+          N/A
+        </div>
+        <div class="conntainer__nav__buttons">
+          <button class="Main-Nav-Button">N/A</button>
+          <button class="Info-Nav-Button">N/A</button>
+          <button class="meme-Nav-Button">N/A</button>
+        </div>
+      <div>
+    </div>
+  </div>
+
+  <!--  Crypto Main/Price  -->
+  <div class="container__main__crypto">
     <div>
+      N/A
+    </div>
+      <div>
+        <div>
+          Price:N/A
+        </div>
+        Graph Placeholder
+      </div>
   </div>
 </body>
 </html>
